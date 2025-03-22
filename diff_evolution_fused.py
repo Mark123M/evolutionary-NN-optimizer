ab4b70ddacc44ccf2a1afbb1cb44386661c61443
User <mark123ymm@gmail.com>
2025-03-22 19:37:12 -0400

    bias

--- diff_evolution_fused.py
+++ diff_evolution_fused.py
@@ -28,21 +28,23 @@ batch_size = 400000
 class DE_NN(nn.Module):
     def __init__(self, NP, CR, F):
         super(DE_NN, self).__init__()
-        self.lin1s = torch.randn((NP, 4, 1), requires_grad=False).to(device)
-        self.lin2s = torch.randn((NP, 8, 4), requires_grad=False).to(device)
-        self.lin3s = torch.randn((NP, 4, 8), requires_grad=False).to(device)
-        self.lin4s = torch.randn((NP, 1, 4), requires_grad=False).to(device)
-        self.layers_len = 4
+        lin1s = torch.randn((NP, 4, 1), requires_grad=False).to(device, non_blocking=True)
+        lin2s = torch.randn((NP, 8, 4), requires_grad=False).to(device, non_blocking=True)
+        lin3s = torch.randn((NP, 4, 8), requires_grad=False).to(device, non_blocking=True)
+        lin4s = torch.randn((NP, 1, 4), requires_grad=False).to(device, non_blocking=True)
+        self.layers = [lin1s, lin2s, lin3s, lin4s]
+        bias1 = torch.randn((NP, 4, 1), requires_grad=False).to(device, non_blocking=True)
+        bias2 = torch.randn((NP, 8, 1), requires_grad=False).to(device, non_blocking=True)
+        bias3 = torch.randn((NP, 4, 1), requires_grad=False).to(device, non_blocking=True)
+        bias4 = torch.randn((NP, 1, 1), requires_grad=False).to(device, non_blocking=True)
+        self.biases = [bias1, bias2, bias3, bias4]
         self.NP = NP
         self.CR = CR
         self.F = F
         self.min_l = float('inf')
         self.best_model = 0
-    def forward_all(self, X):
+    def forward_all(self, X, layers):
         # This is just bmm???
-        X = torch.relu(torch.einsum('lik,lkj->lij', self.lin1s, X))
-        X = torch.relu(torch.einsum('lik,lkj->lij', self.lin2s, X))
-
         #M = torch.empty((NP, 8, batch_size)).to(device) # l, i, j
         #for l in range(NP):
         #    for i in range(8):
@@ -50,30 +52,21 @@ class DE_NN(nn.Module):
         #            total = 0
         #            for k in range(4):
         #                total += self.lin2s[l,i,k] * X[l,k,j]
+        #            total += self.bias2[l, i]
         #            M[l,i,j] = total
         #print(torch.sum(torch.relu(M)))
-
-        X = torch.relu(torch.einsum('lik,lkj->lij', self.lin3s, X))
-        return torch.einsum('lik,lkj->lij', self.lin4s, X)
-    def forward_i(self, X, layers): # a single pass
-        for k in range(self.layers_len - 1):
-            X = torch.relu(layers[k](X))
-        return layers[self.layers_len - 1](X)
-    def forward(self, X):
-        for k in range(self.layers_len - 1):
-            X = torch.relu(self.layers[k][self.best_model](X))
-        return self.layers[self.layers_len - 1][self.best_model](X)
+        for i in range(len(layers) - 1):
+            X = torch.relu(torch.einsum('lik,lkj->lij', layers[i], X) + self.biases[i])
+        X = torch.einsum('lik,lkj->lij', layers[len(layers) - 1], X) + self.biases[len(layers) - 1]
+        return X
     def step(self, X, Y, L, type='param'): # forward pass with candidate i
         nvtx.range_push("forward_1")
-        fx = L(self.forward_all(X), Y).mean(dim = 2)
+        fx = L(self.forward_all(X, self.layers), Y).mean(dim = 2)
         nvtx.range_pop()
         agent_ids = random.sample(range(0, self.NP), 3) # how to efficiently reject self? rej sampling?
         nvtx.range_push(f"copy layers")
-        y1s = torch.randn_like(self.lin1s).to(device, non_blocking=True)
-        y2s = torch.randn_like(self.lin2s).to(device, non_blocking=True)
-        y3s = torch.randn_like(self.lin3s).to(device, non_blocking=True)
-        y4s = torch.randn_like(self.lin4s).to(device, non_blocking=True)
-        print(agent_ids, fx.shape, y1s.shape, y2s.shape, y3s.shape, y4s.shape)
+        y_layers = [torch.randn_like(self.layers[i], requires_grad=False).to(device, non_blocking=True) for i in range(len(self.layers))]
+        #print(agent_ids, fx.shape, y1s.shape, y2s.shape, y3s.shape, y4s.shape)
         nvtx.range_pop()
 
 epochs = 2000
@@ -91,9 +84,9 @@ print(X.shape, Y.shape)
 
 model = DE_NN(NP, CR, F).to(device) 
 model = torch.compile(model, mode='default')
-L = nn.MSELoss(reduction='none').to(device)
+L = nn.MSELoss(reduction='none')
 
-Y_pred = model.forward_all(X)
+Y_pred = model.forward_all(X, model.layers)
 print(Y_pred.shape)
 
 print(sys.argv[1])
@@ -103,10 +96,8 @@ if sys.argv[1] == 'nsight':
         print(model.min_l)
 elif sys.argv[1] == 'chrome':
     with torch.profiler.profile(record_shapes=True, profile_memory=True, with_stack=True) as p:
-        with torch.no_grad():
-            for i in range(model.NP):
-                model.step(i, X, Y, L, 'block')
-            print(model.min_l)
+        model.step(X, Y, L, 'block')
+        print(model.min_l)
     prefix = f"{int(time.time())}"
     p.export_chrome_trace(f"chrome_trace_{prefix}.json.gz")
     p.export_memory_timeline(f"chrome_memory_{prefix}.html")
