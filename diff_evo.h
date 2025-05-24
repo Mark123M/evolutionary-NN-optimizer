619ff4bfb4309a124e36de5cf11e32887b3f1b91
Mark123M <mark123ymm@gmail.com>
2025-05-24 13:54:11 -0400

    kernel debug

--- diff_evo.h
+++
@@ -1,9 +0,0 @@
-#include <torch/extension.h>
-
-// Declaration of the CUDA function
-torch::Tensor square_matrix_cuda(torch::Tensor matrix);
-
-// Wrapper that will be exposed to Python
-inline torch::Tensor square_matrix(torch::Tensor matrix) {
-    return square_matrix_cuda(matrix);
-}
